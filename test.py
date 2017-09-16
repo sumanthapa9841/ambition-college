847a257077c7216ece5ec7f33678d8d8db1513d3
sumanthapa <spiderthapa@gmail.com>
2017-09-17 02:10:05 +0545

    python is commited

---
+++ test.py
@@ -0,0 +1,14 @@
+for i in range(2):
+	for j in range(2):
+		input(x[i][j])
+
+for i in range(2):
+ 	for j in range(2):
+ 		y=x*x
+ 		result[i][j]=y
+
+for i in range(2):
+	for j in range(2):
+		print(result[i][j])
+
+
